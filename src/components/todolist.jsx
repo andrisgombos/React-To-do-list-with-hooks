dd6be554186460268940ecb6f1b5224b2be4089d
Andras <andrisgombos@gmail.com>
2021-08-01 18:33:24 +0200

    adding state changes + setimeout

--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -1,8 +1,13 @@
+import React, { useState } from "react";
 import Todoitem from "./todoitem";
 
 function Todolist() {
 
-    const todos = ["Mastering ReactJS", "Use props", "Create components"]
+    const [todos, setTodos] = useState(["use State", "Use props", "Create components", "Mastering ReactJS"])
+
+    setTimeout(() => {
+        setTodos(["Use props", "Create components", "Mastering ReactJS"])
+        }, 2000);
 
     return (
         <ul>
