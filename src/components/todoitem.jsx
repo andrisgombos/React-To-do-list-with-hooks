582f4010674738fa30b8150a6a030ade9e4680f5
Andras <andrisgombos@gmail.com>
2021-08-11 11:23:38 +0200

    added done and remove functionality

--- src/components/todoitem.jsx
+++ src/components/todoitem.jsx
@@ -1,10 +1,20 @@
-import './todolist.css';
+/* import './todolist.css';
+
 
 function Todoitem(props) {
 
+    
+
     return (
-        <div className="todoitem">{props.todo.text}</div>
+        <div 
+            className="todoitem"
+            style={{ textDecoration: props.todo.completed ? "line-through" : "" }}>
+            {props.todo.text}
+            <div>
+                <button onClick={()=> isCompleted(index)}>Done</button>
+            </div>
+        </div>
     )
 }
 
-export default(Todoitem)
+export default(Todoitem) */
--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -1,14 +1,54 @@
 import React, { useState } from "react";
-import Todoitem from "./todoitem";
 import './todolist.css';
+
+function Todoitem({todo, index, isCompleted, removeTodo}) {
+
+    return (
+        <div 
+            className="todoitem"
+            style={{ textDecoration: todo.completed ? "line-through" : "" }}>
+            {todo.text}
+            <div>
+                <button onClick={()=> isCompleted(index)}>Done</button>
+                <button onClick={()=> removeTodo(index)}>X</button>
+            </div>
+        </div>
+    )
+}
 
 function Todolist() {
 
-    const [todos, setTodos] = useState([{text: "Use State"}, {text: "Use props"}, {text: "Create components"}, {text: "Mastering ReactJS"}]);
+    const [todos, setTodos] = useState([
+        {
+            text: "Use State",
+            completed: false
+        }, {
+            text: "Use props",
+            completed: false
+        }, {
+            text: "Create components",
+            completed: false
+        }, {
+            text: "Mastering ReactJS",
+            completed: false
+        }]);
+
     const [value, setValue] = useState('');
 
     const addTodo = text => {
         const newTodos = [...todos, {text}];
+        setTodos(newTodos)
+    }
+
+    const isCompleted = index => {
+        const newTodos = [...todos];
+        newTodos[index].completed = true;
+        setTodos(newTodos)
+    }
+
+    const removeTodo = index => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
         setTodos(newTodos)
     }
 
@@ -24,8 +64,14 @@ function Todolist() {
         <div className="todolist">
             <form onSubmit={handleSubmit}>
                 <div>
-                    {todos.map(todo => {
-                        return <Todoitem todo={todo} />;
+                    {todos.map((todo, index) => {
+                        return <Todoitem 
+                                key={index} 
+                                index={index} 
+                                todo={todo} 
+                                isCompleted={isCompleted}
+                                removeTodo={removeTodo} 
+                            />;
                         })
                 }
                 </div>
